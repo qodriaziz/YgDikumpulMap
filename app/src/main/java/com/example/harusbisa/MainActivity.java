193a822f3e459314f262ce9158a15e3a54ff51c1
qodriaziz <61813572+qodriaziz@users.noreply.github.com>
2023-06-11 23:58:21 +0700

    edit ui pada xml

--- app/src/main/java/com/example/harusbisa/MainActivity.java
+++ app/src/main/java/com/example/harusbisa/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow()
     }
 }
