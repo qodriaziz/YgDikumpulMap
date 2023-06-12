97b99080bc3edd11f96064bdc702b0b74ed2733c
qodriaziz <61813572+qodriaziz@users.noreply.github.com>
2023-06-12 13:40:38 +0700

    xml search

--- app/src/main/java/com/example/harusbisa/MainActivity.java
+++ app/src/main/java/com/example/harusbisa/MainActivity.java
@@ -7,12 +7,18 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -32,25 +38,39 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     ImageButton buttonL;
+    TextView textView;
 
     LocationManager locationManager;
     SupportMapFragment supportMapFragment;
     FusedLocationProviderClient fusedLocationProviderClient;
 
-    @SuppressLint("MissingInflatedId")
+    SearchView mapSearch;
+    GoogleMap mapKU;
+
+
+    //@SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonL = findViewById(R.id.button_location);
+        textView = findViewById(R.id.text_location);
+
+
+
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_nya);
         fusedLocationProviderClient = (FusedLocationProviderClient) LocationServices.getFusedLocationProviderClient(this);
 
+
+
         Dexter.withContext(getApplicationContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
                 .withListener(new PermissionListener() {
                     @Override
@@ -70,32 +90,51 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }).check();
 
+        //mapSearch = findViewById(R.id.mapLocal);
+
+
+
         buttonL.setOnClickListener(new View.OnClickListener(){
+            @Override
             public void onClick(View v) {
                 getLokasi();
             }
         });
-        Dexter.withContext(getApplicationContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-                .withListener(new PermissionListener() {
-                    @Override
-                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
-                        getCurrentLocation();
-                    }
 
-                    @Override
-                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
 
-                    }
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_nya);
+        //fusedLocationProviderClient = (FusedLocationProviderClient) LocationServices.getFusedLocationProviderClient(this);
+        //Dexter.withContext(getApplicationContext()).withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+        //        .withListener(new PermissionListener() {
+        //            @Override
+        //            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+        //                getCurrentLocation();
+        //            }
 
-                    @Override
-                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
-                        permissionToken.continuePermissionRequest();
+        //            @Override
+        //            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
 
-                    }
-                }).check();
+        //            }
+
+        //            @Override
+        //            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+        //                permissionToken.continuePermissionRequest();
+
+        //            }
+        //        }).check();
     }
 
+    //@SuppressLint("MissingPermission")
+    @SuppressLint("MissingPermission")
     private void getLokasi() {
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, MainActivity.this);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void getCurrentLocation() {
@@ -130,5 +169,21 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, ""+location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_SHORT).show();
+        try{
+            Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            String address = addresses.get(0).getAddressLine(0);
+
+                 textView.setText(address);
+
+        }catch (Exception e){
+                 e.printStackTrace();
+        }
     }
 }
